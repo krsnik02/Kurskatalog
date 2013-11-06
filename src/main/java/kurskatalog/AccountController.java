54a1563cc9b0ca6d5df56e7e31cdf9a6756f65b3
unknown <ben.yuly@gmail.com>
2013-11-05 20:49:23 -0500

    Added accountController file

---
+++ src/main/java/kurskatalog/AccountController.java
@@ -0,0 +1,20 @@
+package kurskatalog;
+
+public class AccountController{
+    
+    String username;
+    String plaintext;
+    
+    Boolean admin;
+	    
+    
+    
+    public String register(){
+	return null;
+    }
+    
+    public String login (){
+	return null;
+    }
+    
+}
