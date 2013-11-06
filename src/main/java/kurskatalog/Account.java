12122c67f676fcf155f273eb42946834b769eb47
Garrett Hartshaw <ghartshaw@gmail.com>
2013-11-05 19:52:32 -0500

    Created Account entity.

---
+++ src/main/java/kurskatalog/Account.java
@@ -0,0 +1,36 @@
+package kurskatalog;
+
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Account
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+
+	private String username;
+	private String password;
+	private boolean admin;
+
+	/* Constructors */
+
+	public Account() {}
+
+	/* Getters/Setters */
+
+	public long getId() { return id; }
+	public void setId( long id_ ) { id = id_; }
+
+	public String getUsername() { return username; }
+	public void setUsername( String username_ ) { username = username_; }
+
+	public String getHashedPassword() { return password; }
+	public void setHashedPassword( String password_ ) { password = password_; }
+
+	public boolean isAdmin() { return admin; }
+	public void setAdmin( boolean admin_ ) { admin = admin_; }
+}
