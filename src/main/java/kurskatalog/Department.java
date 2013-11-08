7b4f8577538234b5a91b1dca94c5a7c48f277ec1
musicisawesome <ben.yuly@gmail.com>
2013-11-07 20:53:13 -0500

    Created Course, Department, and CourseType files

---
+++ src/main/java/kurskatalog/Department.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kurskatalog;
+
+/**
+ *
+ * @author Ben Yuly
+ */
+public class Department {
+    
+}
