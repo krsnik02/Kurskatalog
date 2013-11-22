7cbd8c3a51e5ea35c2a8aa47499e628915be5ae5
musicisawesome <ben.yuly@gmail.com>
2013-11-21 20:39:51 -0500

    added LocationController, create-location.xhtml, and Location EJB

---
+++ src/main/java/kurskatalog/LocationEJB.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kurskatalog;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Ben Yuly
+ */
+
+@Stateless
+public class LocationEJB {
+    
+    @PersistenceContext(unitName = "KurskatalogPU")
+    private EntityManager em;
+
+    
+    public void persist( Location loc )
+    {
+	em.persist( loc );
+    }
+
+    public void remove( Location loc )
+    {
+        em.remove( em.merge( loc ) );
+    }
+    
+    public List<Location> getAllLocations()
+    {
+        return em.createQuery( "SELECT l FROM Location l", Location.class ).getResultList();
+    }
+
+}
