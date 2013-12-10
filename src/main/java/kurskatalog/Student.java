34c1d464ae4279e07c707d45ab987eca0cff5cf3
Garrett Hartshaw <ghartshaw@gmail.com>
2013-12-10 04:05:28 -0500

    Began implementing registration on offerings page.

--- src/main/java/kurskatalog/Student.java
+++ src/main/java/kurskatalog/Student.java
@@ -1,20 +1,29 @@
 package kurskatalog;
 
 import java.util.List;
+import java.util.ArrayList;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
 
 @Entity
 public class Student extends Account
 {
+    @OneToMany(cascade=CascadeType.ALL)
     private List<Offering> registeredCourses;
     
-    public Student() {}
+    public Student() 
+    {
+        registeredCourses = new ArrayList<Offering>();
+    }
     
-    public List<Offering> getRegisteredCourses(){
+    public List<Offering> getRegisteredCourses()
+    {
 	return registeredCourses;
     }
     
-    public void setRegisteredCourses(List<Offering> registeredCourses_){
+    public void setRegisteredCourses( List<Offering> registeredCourses_ )
+    {
 	registeredCourses = registeredCourses_;
     }
 };
